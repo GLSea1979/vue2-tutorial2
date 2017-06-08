e7a740e375d98e002fbb3807aa3b1debaa7de472
GLSea1979 <builtbygl@gmail.com>
2017-06-08 15:06:33 -0700

    second vue tutorial

---
+++ app.js
@@ -0,0 +1,49 @@
+'use strict';
+
+new Vue({
+  el: '#events',
+  // here we can register any values that hold data for the app
+  data: {
+    event: { name: '', description '', date: ''},
+    events: []
+  },
+  // anything within the ready function will run when the app loads
+  ready: function() {
+    this.fetchEvents();
+  },
+  // methods we want to use in our app are registered here
+  methods: {
+    fetchEvents: function() {
+      var events = [
+        {
+          id: 1,
+          name: 'Tiff',
+          description: 'Toronto International Film Festival',
+          date: '2015-09-10'
+        },
+        {
+          id: 2,
+          name: 'The Martian Premiere',
+          description: 'The Martian comes to theatres',
+          date: '2015-10-02'
+        },
+        {
+          id: 3,
+          name: 'SWSX',
+          description: 'Music, film and interactive festival in Austin, TX',
+          date: '2016-03-11'
+        }
+      ];
+      // $set is a convenience method in Vue similar to pushing data
+      // into an array
+      this.$set('events', events);
+    },
+    // adds an event to the existing events array
+    addEvent: function() {
+      if(this.event.name) {
+        this.events.push(this.event);
+        this.event = { name: '', description: '', date: ''};
+      }
+    }
+  }
+});
